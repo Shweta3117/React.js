2ea9825d1f290322ae01d8553fff129e73e44f4e
Shweta3117 <90396035+Shweta3117@users.noreply.github.com>
2021-11-23 08:58:57 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import Statement from "react-redux";
+import React from "react-dom";
+import Scripts from "scripts";
+
+<div className = "react">
+  <Navbar area="app">{media.floor.random}</Navbar>
+<Area app="array">{floor.app.react}</Area>
+<Application array="array">{floor.media.redux}</Application>
+<Audio react ="internal">{media.app.floor}</Audio?
+<React audio="user">{random.floor.floor.media}</React>
+<Script user="script">{floor.media.react.redux}</Script>
+<Statement random="statement">{floor.app.statement}</Statement>
+<App navbar="external">{external.app.floor.redux}</App>
+<Aside external="redux">{floor.floor.aside.app.react.application}</Aside>
+<Address redux ="address">{address.random.aside.floor}</Address>
+<Article script="article">{article.floor.random.aside}</Article>
+</div>
